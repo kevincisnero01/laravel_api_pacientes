3d8cdd9d6b2817248f599119880c17d0d69ab459
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-02-02 23:15:25 -0430

    Modelo, Controlar y Ruta index Pacientes

---
+++ app/Models/Patient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+    'names' ,
+    'last_names' ,
+    'age' ,
+    'gender' ,
+    'ci' ,
+    'blood_type' ,
+    'phone' ,
+    'email' ,
+    'address'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}
